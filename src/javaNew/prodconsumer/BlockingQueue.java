3a1aad3c3fc5bcd22029a08bf1e845d13b083de2
Shailesh <shailesh.makhijani@walmart.com>
2019-05-13 23:20:22 +0530

    Producer consumer
    1. wait notify
    2. Blocking queue

--- src/javaNew/prodconsumer/BlockingQueue.java
+++ src/javaNew/prodconsumer/BlockingQueue.java
@@ -1,6 +1,7 @@
-package javaNew;
+package javaNew.prodconsumer;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * BlockingQueue interface in Java is added in Java 1.5 along with various other concurrent Utility classes like ConcurrentHashMap,
@@ -16,26 +17,23 @@ public class BlockingQueue {
     private List queue = new LinkedList();
     private int limit = 10;
 
-    public BlockingQueue(int limit)
-    {
+    public BlockingQueue(int limit) {
         this.limit = limit;
     }
 
     public synchronized void enqueue(Object item)
-            throws InterruptedException
-    {
+            throws InterruptedException {
         while (this.queue.size() == this.limit) {
             wait();
         }
         if (this.queue.size() == 0) {
             notifyAll();
         }
-        this.queue.add((String)item);
+        this.queue.add( item);
     }
 
     public synchronized Object dequeue()
-            throws InterruptedException
-    {
+            throws InterruptedException {
         while (this.queue.size() == 0) {
             wait();
         }
@@ -44,5 +42,43 @@ public class BlockingQueue {
         }
 
         return this.queue.remove(0);
+    }
+
+
+    public static void main(String args[]) {
+        BlockingQueue queue = new BlockingQueue(5);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i <= 4; i++) {
+                    try {
+                        queue.enqueue(i);
+                        TimeUnit.MILLISECONDS.sleep(100);
+                        System.out.println("enqueue : " + i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Integer val = (Integer) queue.dequeue();
+                        System.out.println("dequeue" + val);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
     }
 }
